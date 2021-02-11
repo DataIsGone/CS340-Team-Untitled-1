fa61c2f2ed729781becd1d491ade724c086e170d
Angel Tapia <tapiaang@oregonstate.edu>
2021-02-10 21:21:47 -0800

    Added data manipulation sql file

--- createAndFillTables.sql
+++ createAndFillTables.sql
@@ -121,21 +121,3 @@ INSERT INTO keyboardOrders (orderNum, keyboardNum, quantityOrdered, pricePerUnit
 VALUES (1, 1, 2, 500.00),
 	   (2, 3, 1, 300.00),
 	   (3, 1, 50, 500.00);
-
-
--- update dummy data, example 1 (`keyboards` entity)
-UPDATE keyboards
-SET quantityInStock = 48
-WHERE name = "Flaming Hot Cheeto";
-
-
--- update dummy data, example 2 (`orders` entity)
-UPDATE orders
-SET orderDate = "2021-09-25"
-WHERE customerNum = 2;
-
-
--- update dummy data, example 3 (`customers` entity)
-UPDATE customers
-SET phoneNumber = NULL
-WHERE lastName = "Champ" AND firstName = "Pog";
